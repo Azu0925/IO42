7edda6404570e61b7ced9c84b1861f78c10d5916
Azu0925 <yamaneko564783@gmail.com>
2022-06-20 00:56:03 +0900

    たぶんできた

--- No.4/Light.py
+++ No.4/Light.py
@@ -1,7 +1,6 @@
 import time
 
 class Light: 
-
   def __init__(self, led_pin, light_state, switch_state):
     self.LED_PIN = led_pin
     self.light_state = light_state
@@ -38,18 +37,26 @@ class Light:
 
   def alternate(self, on_delay_time=0, off_delay_time=0):
     if self.switch_state == 0 and self.is_pushing == False and self.light_state == False:
+      print('??')
       self.on_delay_timer = time.time() + on_delay_time 
       self.is_pushing = True
     elif self.switch_state == 0 and self.is_pushing == False and self.light_state == True:
-      self.on_delay_timer = time.time() + on_delay_time
+      print('##')
+      self.off_delay_timer = time.time() + off_delay_time
       self.is_pushing = True
     elif self.switch_state == 0 and self.is_pushing == True:
+      print('--')
       pass
     elif self.switch_state == 1 and self.is_pushing == True:
+      print('離した')
       self.is_pushing = False
 
     if self.on_delay_timer is not None:
       if self.on_delay_timer <= time.time():
-        self.light_state = not self.light_state
+        self.light_state = True
         self.on_delay_timer = None
-        print(self.on_delay_timer) 
+
+    if self.off_delay_timer is not None:
+      if self.off_delay_timer <= time.time():
+        self.light_state = False
+        self.off_delay_timer = None
