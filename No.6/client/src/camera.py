db9558e2f5ebd5112500c7c0ed2bf5f3e23cdd01
Azu0925 <yamaneko564783@gmail.com>
2022-08-08 15:29:07 +0900

    リファクタ

---
+++ No.6/client/src/camera.py
@@ -0,0 +1,22 @@
+import cv2
+import datetime
+
+def record(file_path):
+    cap = cv2.VideoCapture(0)
+    fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    fps = 20.0
+    size = (640, 360)
+    limit = datetime.datetime.now() + datetime.timedelta(seconds=10)
+    writer = cv2.VideoWriter(file_path, fmt, fps, size)
+
+    while True:
+        _, frame = cap.read()
+        frame = cv2.resize(frame, size)
+        writer.write(frame)
+
+        if datetime.datetime.now() >= limit:
+            break
+    
+    writer.release()
+    cap.release()
+    cv2.destroyAllWindows()
--- No.6/client/src/main.py
+++ No.6/client/src/main.py
@@ -1,32 +1,15 @@
-from fileinput import filename
 import drive
 import os
-import cv2
 import datetime
+import camera
 
 def main():
     base = os.path.dirname(os.path.abspath(__file__))
-    cap = cv2.VideoCapture(0)
-    fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    fps = 20.0
-    size = (640, 360)
     file_name = 'test_' + str(datetime.datetime.now()) + '.m4v'
     file_info = os.path.normpath(os.path.join(base, '../video/' + file_name))
-    writer = cv2.VideoWriter(file_info, fmt, fps, size)
-    limit = datetime.datetime.now() + datetime.timedelta(seconds=10)
     
-    while True:
-        _, frame = cap.read()
-        frame = cv2.resize(frame, size)
-        writer.write(frame)
-        # cv2.imshow('frame', frame)
-
-        if datetime.datetime.now() >= limit:
-            break
-    
-    writer.release()
-    cap.release()
-    cv2.destroyAllWindows()
+    # recording
+    camera.record(file_info)
 
     # upload to Google drive
     drive.upload(file_info, file_name)
